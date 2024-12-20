7aa8c6561f3596617af4f9e6d41e40cc029e9f7d
jascharan-singh <singhjascharan@yahoo.com>
2024-12-20 15:21:22 -0500

    Just intro

---
+++ My-react-app/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+        <footer>
+            <p>&copy;{new Date().getFullYear()} Your website 2024</p>
+        </footer>
+    )
+}
+export default Footer
---
+++ My-react-app/src/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                
+            </nav>
+        </header>
+    )
+}
+export default Header
