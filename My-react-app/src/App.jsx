6008b3907920be4f248a996af9deae98c49c8df7
jascharan-singh <singhjascharan@yahoo.com>
2024-12-24 22:29:45 -0500

    onClick

--- My-react-app/src/App.jsx
+++ My-react-app/src/App.jsx
@@ -1,24 +1,7 @@
-import List from "./List"
-
-
-
-
+import Button from "./Button"
 function App() {
-  const fruits = [
-    {id: 1, name:"apple",  calories: 100}, {id : 2, name:"banana", calories: 200},
-    {id : 3, name:"orange", calories: 300}, {id : 4, name:"pineapple", calories: 400},
-     {id : 5, name:"cherry", calories: 500}
-    ];
-  const vegetables = [{id: 6, name: "potatoes", calories: 110},
-      {id: 7, name: "celery", calories: 15},
-      {id: 8, name: "carrots", calories: 25}, 
-      {id: 9, name: "corn", calories: 63}, 
-      {id: 10, name: "broccoli", calories: 50}];
   return (
-    <>
-    {fruits.length > 0 && <List items={fruits} catagories={'Fruits'}/>} 
-    {vegetables.length > 0 && <List items={vegetables} catagories={'Vegetables'}/>} 
-    </>
+  <Button/>
   )
 }
 
--- My-react-app/src/Button.jsx
+++ My-react-app/src/Button.jsx
@@ -1,9 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-function Button(props){
+function Button(){
+    const handleClick=(e)=>{
+        console.log(e.target.textContent = 'OUCH!')
+    }
     return(
-        <button className="btn btn-outline-primary my-custom-btn">{props.nam}</button>
+        <button onClick={(e)=>handleClick(e)} className="btn btn-outline-primary my-custom-btn">Click me </button>
     )
 }
 export default Button
